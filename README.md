0ef8579344bd044daaee19e549ee75b7b9087950
korzepadawid <korzepadawid@yahoo.com>
2021-09-13 09:39:47 +0200

    feat: init

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-quora-clone
---
+++ src/main/java/io/github/korzepadawid/springquoraclone/SpringQuoraCloneApplication.java
@@ -0,0 +1,13 @@
+package io.github.korzepadawid.springquoraclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuoraCloneApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringQuoraCloneApplication.class, args);
+  }
+
+}
