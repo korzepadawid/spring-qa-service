efd44ffe2bf201fe217b839cbf978553e1e46e7c
korzepadawid <korzepadawid@yahoo.com>
2021-09-13 10:59:40 +0200

    feat: security config

--- src/main/java/io/github/korzepadawid/springquoraclone/repository/sql/AppUserRepositorySql.java
+++ src/main/java/io/github/korzepadawid/springquoraclone/repository/sql/AppUserRepositorySql.java
@@ -1,12 +1,13 @@
 package io.github.korzepadawid.springquoraclone.repository.sql;
 
 import io.github.korzepadawid.springquoraclone.model.AppUser;
+import io.github.korzepadawid.springquoraclone.repository.AppUserRepository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface AppUserRepositorySql extends CrudRepository<AppUser, Long> {
+public interface AppUserRepositorySql extends AppUserRepository, CrudRepository<AppUser, Long> {
 
   @Query("select a from AppUser a where lower(a.username) = lower(:string) or "
       + "lower(a.email) = lower(:string) ")
