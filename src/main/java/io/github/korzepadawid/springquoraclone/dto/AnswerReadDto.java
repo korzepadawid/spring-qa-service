b4754261617f06e27eaa4f47d446816c4d72d6ad
korzepadawid <korzepadawid@yahoo.com>
2021-09-21 16:02:21 +0200

    feat: voting statistics in AnswerReadDto

--- src/main/java/io/github/korzepadawid/springquoraclone/dto/AnswerReadDto.java
+++ src/main/java/io/github/korzepadawid/springquoraclone/dto/AnswerReadDto.java
@@ -1,6 +1,7 @@
 package io.github.korzepadawid.springquoraclone.dto;
 
 import io.github.korzepadawid.springquoraclone.model.Answer;
+import io.github.korzepadawid.springquoraclone.model.VoteType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,9 +19,17 @@ public class AnswerReadDto {
   private Long id;
   private String text;
   private AppUserReadDto author;
+  private Integer upVotes = 0;
+  private Integer downVotes = 0;
 
   public AnswerReadDto(Answer answer) {
-    BeanUtils.copyProperties(answer, this, "author");
+    BeanUtils.copyProperties(answer, this, "author", "upVotes", "downVotes");
     this.author = new AppUserReadDto(answer.getAuthor());
+    answer.getVotes().forEach(vote -> {
+      this.upVotes =
+          vote.getVoteType().equals(VoteType.UP_VOTE) ? this.upVotes + 1 : this.upVotes;
+      this.downVotes =
+          vote.getVoteType().equals(VoteType.DOWN_VOTE) ? this.downVotes + 1 : this.downVotes;
+    });
   }
 }
--- src/main/java/io/github/korzepadawid/springquoraclone/repository/sql/AnswerRepositorySql.java
+++ src/main/java/io/github/korzepadawid/springquoraclone/repository/sql/AnswerRepositorySql.java
@@ -6,11 +6,15 @@ import io.github.korzepadawid.springquoraclone.model.Question;
 import io.github.korzepadawid.springquoraclone.repository.AnswerRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface AnswerRepositorySql extends AnswerRepository, CrudRepository<Answer, Long> {
 
-  List<Answer> findByQuestion(Question question);
+  @Query("select a from Answer a left join fetch a.votes v join fetch a.author a1 "
+      + "where a.question = :question")
+  List<Answer> findByQuestion(@Param("question") Question question);
 
   Optional<Answer> findByIdAndAuthor(Long id, AppUser author);
 }
